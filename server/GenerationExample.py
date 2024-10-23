0d626dccf46075cf09b82bcbb6f05b0b71707a7c
Anonyo <32406693+anonyosmitra@users.noreply.github.com>
2024-10-23 11:28:50 -0700

    Update GenerationExample.py
    Added front end input sample

--- server/GenerationExample.py
+++ server/GenerationExample.py
@@ -1,3 +1,5 @@
 from server.RecipeModel import generate
 
 generate(["bread","onion","apples 2"])
+
+# [{"name":"bread","quantity":""},{"name":"onion","quantity":""},{"name":"apples","quantity":"2"}]
