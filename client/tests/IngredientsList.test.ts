7113384fbcf52819eccb1cd9d159f60b88f94637
Nikita Toropov <toropov.nikita2004@gmail.com>
2024-11-06 23:06:28 +0100

    Add input bindings test

--- client/tests/IngredientsList.test.ts
+++ client/tests/IngredientsList.test.ts
@@ -1,40 +1,68 @@
-import { mount } from "@vue/test-utils"
-import { describe, expect, test, vi } from "vitest"
+import { DOMWrapper, mount, VueWrapper } from "@vue/test-utils"
+import { beforeEach, describe, expect, test, vi } from "vitest"
 import IngredientsList from "../src/App.vue"
 
 describe("IngredientsList.vue", () => {
-    test("add ingredient", async () => {
-        const wrapper = mount(IngredientsList)
-        const ingredients = wrapper.find("ul.ingredients").element
-        ingredients.scroll = vi.fn()
+    let wrapper: VueWrapper
+    let ingredients: HTMLUListElement
 
-        const button = wrapper.find("button.ingredients__button--add")
+    beforeEach(() => {
+        wrapper = mount(IngredientsList)
+        ingredients = wrapper.find("ul.ingredients").element as HTMLUListElement
+        ingredients.scroll = vi.fn()
+    })
+
+    test("add ingredients", async () => {
+        const addButton = wrapper.find("button.ingredients__button--add")
         const count = 10
-        for (let i = 0; i < count; ++i) {
-            await button.trigger("click")
-        }
+        await clickButton(addButton, count)
 
         expect(ingredients.childElementCount).toBe(count + 1)
         expect(ingredients.scroll).toHaveBeenCalledTimes(count)
     })
 
-    test("remove ingredient", async () => {
-        const wrapper = mount(IngredientsList)
-        const ingredients = wrapper.find("ul.ingredients").element
-        ingredients.scroll = vi.fn()
+    test("remove ingredients", async () => {
+        const addButton = wrapper.find("button.ingredients__button--add")
+        const addCount = 10
+        await clickButton(addButton, addCount)
 
-        const add_button = wrapper.find("button.ingredients__button--add")
-        const add_count = 10
-        for (let i = 0; i < add_count; ++i) {
-            await add_button.trigger("click")
-        }
+        const removeButton = wrapper.find("button.ingredients__button--remove")
+        const removeCount = 3
+        await clickButton(removeButton, removeCount)
 
-        const remove_button = wrapper.find("button.ingredients__button--remove")
-        const remove_count = 3
-        for (let i = 0; i < remove_count; ++i) {
-            await remove_button.trigger("click")
-        }
-
-        expect(ingredients.childElementCount).toBe(add_count - remove_count + 1)
+        expect(ingredients.childElementCount).toBe(addCount - removeCount + 1)
     })
+
+    test("enter the ingredient information", async () => {
+        const addButton = wrapper.find("button.ingredients__button--add")
+        await clickButton(addButton, 1)
+
+        const ingredientNameInput = wrapper.find("input[name=ingredient-name]")
+        // prettier-ignore
+        const ingredientQuantityInput = wrapper.find("input[name=ingredient-quantity]")
+
+        // prettier-ignore
+        expect((ingredientNameInput.element as HTMLInputElement).value).toBe("")
+        // prettier-ignore
+        expect((ingredientQuantityInput.element as HTMLInputElement).value).toBe("")
+
+        const ingredientName = "Bread"
+        await ingredientNameInput.setValue(ingredientName)
+        const ingredientQuantity = "1 loaf"
+        await ingredientQuantityInput.setValue(ingredientQuantity)
+
+        // prettier-ignore
+        expect((ingredientNameInput.element as HTMLInputElement).value).toBe(ingredientName)
+        // prettier-ignore
+        expect((ingredientQuantityInput.element as HTMLInputElement).value).toBe(ingredientQuantity)
+    })
+
+    async function clickButton(
+        button: DOMWrapper<Element>,
+        count: number
+    ): Promise<void> {
+        for (let i = 0; i < count; ++i) {
+            await button.trigger("click")
+        }
+    }
 })
